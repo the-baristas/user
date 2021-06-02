f984f1abcc11cc04429cd062e63263f799b47b61
anthsirimar <anthony.sirimarco@smoothstack.com>
2021-06-01 20:30:35 -0700

    updated controller

--- src/main/java/com/ss/utopia/controller/UserController.java
+++ src/main/java/com/ss/utopia/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ import com.ss.utopia.service.UserService;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(exposedHeaders = {"Authorization", "Access-Control-Allow-Origin"})
 public class UserController {
 
 	@Autowired
