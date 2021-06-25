6f2459bf8c48b80d793a02cc61e941ac2335bdfd
anthsirimar <anthony.sirimarco@smoothstack.com>
2021-06-25 12:25:05 -0700

    Added controller advice for exception handling

---
+++ src/main/java/com/ss/utopia/exception/ApplicationExceptionHandler.java
@@ -0,0 +1,70 @@
+package com.ss.utopia.exception;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+	
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    ResponseEntity<String> handleSqlException(SQLException exception) {
+        System.out.printf("An unknown error occurred.", exception);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+    
+    @ExceptionHandler(ResponseStatusException.class)
+    ResponseEntity<Void> handleResponseStatusException(
+            ResponseStatusException exception) {
+        throw exception;
+    }
+    
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Error methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<org.springframework.validation.FieldError> fieldErrors = result.getFieldErrors();
+        return processFieldErrors(fieldErrors);
+    }
+    private Error processFieldErrors(List<org.springframework.validation.FieldError> fieldErrors) {
+        Error error = new Error(HttpStatus.BAD_REQUEST.value(), fieldErrors.get(0).getDefaultMessage());
+        return error;
+    }
+    static class Error {
+        private final int status;
+        private final String message;
+        
+        Error(int status, String message) {
+            this.status = status;
+            this.message = message;
+        }
+        public int getStatus() {
+            return status;
+        }
+        public String getMessage() {
+            return message;
+        }
+
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleUncaughtException(Exception exception) {
+        System.out.printf("An unknown error occurred.", exception);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+}
